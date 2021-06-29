4a37e68f414d1cc45e6fff17bb5488fda18f9883
Alex Yersh <yersh.alex@gmail.com>
2021-06-29 12:37:51 +0300

    rename project AND add new class

---
+++ src/main/Alex/Yersh/BaseEntity.java
@@ -0,0 +1,18 @@
+public class BaseEntity{
+    private long id;
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
