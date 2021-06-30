321ac2a761b86fddb79dadba0354671cc4145e77
Alex Yersh <yersh.alex@gmail.com>
2021-06-30 15:20:27 +0300

    Person class adding

---
+++ src/main/Alex/Yersh/Person.java
@@ -0,0 +1,20 @@
+class Person extends BaseEntity {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
